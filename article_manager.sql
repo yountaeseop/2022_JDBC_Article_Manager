1e2da7fb6e94c4a0c11a8695e278c5096047ec6a
yountaeseop <jangmj6931@gmail.com>
2022-08-10 00:51:17 +0900

    JDBC INSERT 테스트

---
+++ article_manager.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS article_manager;
+CREATE DATABASE article_manager;
+DROP TABLE article;
+
+CREATE TABLE article(
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	title CHAR(100) NOT NULL,
+	`body` TEXT NOT NULL
+);
+
+DESC article;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목',RAND()),
+`body` = CONCAT('내용',RAND());
+
+SELECT * FROM article;
+
+#select rand()
+
+
--- src/JAM/test/JDBCConnectTEST.java
+++ src/JAM/test/JDBCConnectTEST.java
@@ -2,20 +2,36 @@ package JAM.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JDBCTEST {
+public class JDBCConnectTEST {
 	public static void main(String[] args) {
 
 		Connection conn = null;
-
+		PreparedStatement pstmt = null;
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
 			conn = DriverManager.getConnection(url, "root", "5420");
 			System.out.println("연결 성공!");
-
+			
+			String sql = "INSERT INTO article";
+			sql	+= " SET regDate = NOW()";
+			sql	+= ", updateDate = NOW()";
+			sql	+= ", title = CONCAT('제목',RAND())";
+			sql	+= ", `body` = CONCAT('내용',RAND());";
+			
+			System.out.println(sql);
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			int affectedRows = pstmt.executeUpdate();
+			
+			System.out.println("affectedRows : " + affectedRows);
+			
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
 		} catch (SQLException e) {
@@ -28,7 +44,13 @@ public class JDBCTEST {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-
 	}
 }
