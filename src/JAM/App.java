7d6afbce0f4743d0ec634d2c3eeba77da147b2b5
yountaeseop <jangmj6931@gmail.com>
2022-08-14 15:45:59 +0900

    게시물 상세보기 기능 구현 LocalDateTime

--- src/JAM/App.java
+++ src/JAM/App.java
@@ -111,7 +111,34 @@ public class App {
 			
 			System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
 			
-		}else if (cmd.startsWith("article modify ")) {
+		}else if (cmd.startsWith("article detail ")) {
+			int id = Integer.parseInt(cmd.split(" ")[2]);
+			
+			SecSql sql = new SecSql();
+			sql.append("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
+			
+			Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+			
+			if(articleMap.isEmpty()) {
+				System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+				return 0;
+			}
+			
+			System.out.printf("== %d번 게시물 상세보기 ==\n", id);
+			
+			Article article = new Article(articleMap);
+			
+			System.out.printf("번호 : %d\n", article.id);
+			System.out.printf("작성날짜 : %s\n", article.regDate);
+			System.out.printf("수정날짜 : %s\n", article.updateDate);
+			System.out.printf("제목 : %s\n", article.title);
+			System.out.printf("내용 : %s\n", article.body);
+			
+			System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
+			
+		} else if (cmd.startsWith("article modify ")) {
 			int id = Integer.parseInt(cmd.split(" ")[2]);
 			
 			System.out.printf("== %d번 게시물 수정 ==\n", id);
--- src/JAM/Article.java
+++ src/JAM/Article.java
@@ -1,30 +1,23 @@
 package JAM;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 public class Article extends Object {
 
 	public int id;
-	public String regDate;
-	public String updateDate;
+	public LocalDateTime regDate;
+	public LocalDateTime updateDate;
 	public String title;
 	public String body;
-	
-	public Article(int id, String regDate, String updateDate, String title, String body) {
-		this.id = id;
-		this.regDate = regDate;
-		this.updateDate = updateDate;
-		this.title = title;
-		this.body = body;
-	}
-
 
 	public Article(Map<String, Object> articleMap) {
 		this.id = (int)articleMap.get("id");
+		this.regDate = (LocalDateTime)articleMap.get("regDate");
+		this.updateDate = (LocalDateTime)articleMap.get("updateDate");
 		this.title = (String)articleMap.get("title");
 		this.body = (String)articleMap.get("body");
 	}
-
 
 	@Override
 	public String toString() {
