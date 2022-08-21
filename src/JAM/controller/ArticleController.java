fcce453ab9994897c31e6cd82927f2e44dbbb7b1
yountaeseop <jangmj6931@gmail.com>
2022-08-21 11:54:52 +0900

    게시물 수정, 삭제시 권한 체크

--- src/JAM/controller/ArticleController.java
+++ src/JAM/controller/ArticleController.java
@@ -42,10 +42,15 @@ public class ArticleController extends Controller {
 		
 		int id = Integer.parseInt(cmd.split(" ")[2]);
 		
-		boolean isArticleExists = articleService.isArticleExists(id);
+		Article article = articleService.getArticleById(id);
 		
-		if(isArticleExists == false) {
+		if(article == null) {
 			System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+			return;
+		}
+		
+		if(article.memberId != Container.session.loginedMemberId) {
+			System.out.println("해당 게시글에 대한 권한이 없습니다.");
 			return;
 		}
 		
@@ -101,6 +106,11 @@ public class ArticleController extends Controller {
 			return;
 		}
 		
+		if(article.memberId != Container.session.loginedMemberId) {
+			System.out.println("해당 게시글에 대한 권한이 없습니다.");
+			return;
+		}
+		
 		System.out.printf("== %d번 게시물 수정 ==\n", id);
 		System.out.printf("새 제목 : ");
 		String title = sc.nextLine();
@@ -112,6 +122,7 @@ public class ArticleController extends Controller {
 		System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
 		
 	}
+	
 	public void showList(String cmd) {
 		System.out.println("== 게시물 리스트 ==");
 
